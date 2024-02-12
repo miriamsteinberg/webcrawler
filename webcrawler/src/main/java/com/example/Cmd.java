ad7bc3c51c7d0779aac04f7af72985450879016d
miriamsteinberg <104499607+miriamsteinberg@users.noreply.github.com>
2024-02-12 14:54:32 +0200

    Update Cmd.java

--- webcrawler/src/main/java/com/example/Cmd.java
+++ webcrawler/src/main/java/com/example/Cmd.java
@@ -1,23 +1,13 @@
 package com.example;
 
-import com.example.webcrawler.WebPageReader;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Cmd implements CommandLineRunner {
-    private final WebPageReader webPageReader;
-
-
-    public Cmd( WebPageReader webPageReader) {
-        this.webPageReader = webPageReader;
-    }
 
     @Override
     public void run(String... args) {
         // Add your code here that you want to run when the application starts
-        String url = "www.hamichlol.org.il/"; // Replace with the desired URL
-        webPageReader.findLinks(url, 3, 1);
-
     }
 }
